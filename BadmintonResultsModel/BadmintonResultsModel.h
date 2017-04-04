4edc2cf50b6325244b3e6dce5499df4b91aebcec
anfriis <af@andersfr.dk>
2017-04-04 23:40:20 +0200

    Add Xcode project

---
+++ BadmintonResultsModel/BadmintonResultsModel.h
@@ -0,0 +1,19 @@
+//
+//  BadmintonResultsModel.h
+//  BadmintonResultsModel
+//
+//  Created by Anders Friis on 04/04/2017.
+//  Copyright © 2017 Cromian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BadmintonResultsModel.
+FOUNDATION_EXPORT double BadmintonResultsModelVersionNumber;
+
+//! Project version string for BadmintonResultsModel.
+FOUNDATION_EXPORT const unsigned char BadmintonResultsModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BadmintonResultsModel/PublicHeader.h>
+
+
